d854fdcd91e8346955b02de14eedffe53503619a
cksckckcks <chan679999@naver.com>
2024-09-03 18:15:37 +0900

    feat: 구급대 로그인 페이지 구현

--- app/app/src/main/java/com/example/sos/LoginUserActivity.kt
+++ app/app/src/main/java/com/example/sos/LoginUserActivity.kt
@@ -5,25 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_user)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        if (!checkLoginStatus()) {
-            val intent = Intent(this, LoginUserActivity::class.java)
-            startActivity(intent) // 로그인이 안되어 있을 경우 LoginUserActivity를 띄움.
-            finish()
-        }
-    }
-    private fun checkLoginStatus(): Boolean { // 추후 로그인 상태를 체크 해줄 함수
-        return false
     }
 }
