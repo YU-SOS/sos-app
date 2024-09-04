9e739d2d7433aa3b8f87d7ed5602f1e86f61cd48
cksckckcks <chan679999@naver.com>
2024-09-04 15:02:42 +0900

    feat: 회원가입 버튼 추가

--- app/app/src/main/java/com/example/sos/LoginUserActivity.kt
+++ app/app/src/main/java/com/example/sos/LoginUserActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
 
 class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,12 @@ class LoginUserActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 회원가입 버튼 클릭 리스너 설정
+        //val registerButton = findViewById<Button>(R.id.btn_register)
+        //registerButton.setOnClickListener {
+        //    val intent = Intent(this, RegisterAmbulanceActivity::class.java) //
+        //    startActivity(intent)
+        //}
     }
 }
