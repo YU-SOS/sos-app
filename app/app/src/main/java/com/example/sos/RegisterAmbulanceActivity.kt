bda03c975dc7f86731cae18e04839c40e437d05d
cksckckcks <chan679999@naver.com>
2024-09-06 18:33:02 +0900

    feat: 구급대 회원가입 페이지 구현

--- app/app/src/main/java/com/example/sos/RegisterAmbulanceActivity.kt
+++ app/app/src/main/java/com/example/sos/RegisterAmbulanceActivity.kt
@@ -1,28 +1,20 @@
 package com.example.sos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginAmbulanceActivity : AppCompatActivity() {
+class RegisterAmbulanceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_ambulance)
+        setContentView(R.layout.activity_register_ambulance)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        // 회원가입 버튼 클릭 리스너 설정
-        val registerButton = findViewById<Button>(R.id.btn_register)
-        registerButton.setOnClickListener {
-            val intent = Intent(this, RegisterAmbulanceActivity::class.java)
-            startActivity(intent)
         }
     }
 }
