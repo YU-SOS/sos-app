5c5c9348bdaf1ae8f2d386501a1a1e3563ba1921
cksckckcks <chan679999@naver.com>
2024-09-20 14:16:22 +0900

    feat: 카카오맵 기본 코드 작성

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.kakao.maps.open:android:2.11.9") // 카카오맵 api
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
