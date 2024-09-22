33e4660ddab674faa2182507581aaa42c3c3f048
cksckckcks <chan679999@naver.com>
2024-09-22 19:22:02 +0900

    feat: JWT를 이용한 회원가입 로그인 틀 개발

--- app/app/src/main/java/com/example/sos/MainActivity.kt
+++ app/app/src/main/java/com/example/sos/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.example.sos
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.jwt.JWT
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         if (!checkLoginStatus()) {
             val intent = Intent(this, SelectLoginActivity::class.java)
-            startActivity(intent) // 로그인이 안되어 있을 경우 SelectLoginActivity를 띄움.
+            startActivity(intent)
             finish()
         }
     }
-    private fun checkLoginStatus(): Boolean { // 추후 로그인 상태를 체크 해줄 함수
-        return false
+
+    // 토큰 유효성 검사하는 함수
+    private fun isTokenExpired(token: String): Boolean {
+        val jwt = JWT(token)
+        return jwt.isExpired(0)
+    }
+    // 로그인이 되어있는지 체크하는 함수
+    private fun checkLoginStatus(): Boolean {
+        val sharedPreferences = getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("jwt_token", null)
+
+        return token != null && !isTokenExpired(token)
     }
 }
